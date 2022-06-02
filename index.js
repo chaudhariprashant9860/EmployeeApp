c465cff1834e1e81db1fa7cda72a39fa623c3ee6
chaudhariprashant9860 <chaudhariprashant00@gmail.com>
2022-06-02 16:49:06 +0530

    APp changes

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hellow World");
+//Importing express library for create server
+const  express = require("express") 
+
+const app = express()
+
+app.get("/user",(req,res)=>{
+
+    res.send("first Api call")
+
+})
+const port = 5000;
+
+app.listen(port,(req,res)=>{
+
+    console.log(`starting the application on port : http://localhost:${port}`)
+})
